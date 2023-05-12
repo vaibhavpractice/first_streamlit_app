dae8a91f8b19cf383c12c7c1d7109ff455608fa3
vaibhavpractice <133123153+vaibhavpractice@users.noreply.github.com>
2023-05-12 11:23:49 +0530

    Update streamlit_app.py
    changed message of text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
