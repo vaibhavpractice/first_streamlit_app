d70c36b9a184632031d79c3788877b5e11382c9e
vaibhavpractice <133123153+vaibhavpractice@users.noreply.github.com>
2023-05-12 14:15:36 +0530

    Update streamlit_app.py
    modified structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#New section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json()) # writes data to the screen 
-
-# take the json version of the response and normalized it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# gives output in the form of table
-streamlit.dataframe(fruityvice_normalized)
+ # streamlit.text(fruityvice_response.json()) # writes data to the screen 
 
 #don't run anything past here while we truubleshoot
 streamlit.stop()
